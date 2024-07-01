4c2e4e5a3a663d340b27a56eec6b376384b3b1dc
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-07-01 19:46:15 +0200

    critical section

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ COPY . .
 RUN CGO_ENABLED=0 go build .
 
 FROM alpine:3.19.1
+RUN apk update && apk add btrfs-progs
 COPY --from=go-app /data/mt-backup-manager /.
 EXPOSE 8080
 CMD ["/mt-backup-manager"]
--- main.go
+++ main.go
@@ -1,13 +1,50 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
 
 func main() {
 	http.HandleFunc("/", streamBackup)
 
+	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-func streamBackup(w http.ResponseWriter, r *http.Request) {
+var streamCount = 0
+var lock = sync.Mutex{}
 
+func create() {
+	lock.Lock()
+	defer lock.Unlock()
+
+	streamCount++
+	if streamCount > 1 {
+		// already active
+		return
+	}
+
+	// create snapshot
+	// TODO
+}
+
+func release() {
+	lock.Lock()
+	defer lock.Unlock()
+
+	streamCount--
+	if streamCount > 0 {
+		// still active backup streams
+		return
+	}
+
+	// remove snapshot
+	// TODO
+}
+
+func streamBackup(w http.ResponseWriter, r *http.Request) {
+	create()
+	defer release()
 }
